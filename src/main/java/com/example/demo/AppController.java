8877fe963bbddc75a0b3ce07dfb25d75a96b6399
piraveena <piraveena18@gmail.com>
2021-10-16 23:58:29 +0530

    Add springboot app

---
+++ src/main/java/com/example/demo/AppController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class AppController {
+    @GetMapping("/")
+    public String currentUserName(Model model, Authentication authentication) {
+
+        DefaultOidcUser userDetails = (DefaultOidcUser) authentication.getPrincipal();
+        model.addAttribute("userName", userDetails.getName());
+        model.addAttribute("IDTokenClaims", userDetails);
+        return "home";
+    }
+
+}
